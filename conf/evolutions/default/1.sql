8a04b33aed6ccea2239e0a9f018f81783ee1d137
cryuya <r-sasaki@croooober.co.jp>
2022-05-13 17:23:55 +0900

    スペース修正

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,13 +3,13 @@
 -- !Ups
 
 CREATE TABLE comments (
-    id int NOT NULL AUTO_INCREMENT,
-    name varchar(20) NOT NULL,
-    title varchar(255) NOT NULL,
-    comment varchar(255) NOT NULL,
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(20) NOT NULL,
+	title varchar(255) NOT NULL,
+	comment varchar(255) NOT NULL,
 	created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
-    PRIMARY KEY (id)
+	updated_at TIMESTAMP NOT NULL,
+	PRIMARY KEY (id)
 );
 
 INSERT INTO comments VALUES (DEFAULT, 'firstname', 'firstplay', 'firstcomment', CURRENT_TIME, CURRENT_TIME);
