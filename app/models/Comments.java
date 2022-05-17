3333e5244028319a2031695a42539bde6e2ad24e
cryuya <r-sasaki@croooober.co.jp>
2022-05-17 17:24:43 +0900

    modelの変更

--- app/models/Comments.java
+++ app/models/Comments.java
@@ -2,69 +2,30 @@ package models;
 
 import play.data.validation.Constraints;
 import io.ebean.Model;
-import io.ebean.Finder;
-import javax.persistence.Id;
+
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 
 @Entity
 public class Comments extends Model {
-	public static Finder<Long, Comments> finder = new Finder<>(Comments.class);
+	@Id
+	public Integer id;
+	@Constraints.Required 
+	public String name;
+	@Constraints.Required 
+	public String title;
+	@Constraints.Required 
+	public String comment;
 
-	@Id 
-	private Integer id;
-	@Constraints.Required 
-	private String name;
-	@Constraints.Required 
-	private String title;
-	@Constraints.Required 
-	private String comment;
-	@Constraints.Required 
-	private String createdAt;
-	@Constraints.Required 
-	private String updatedAt;
+	public String createdAt;
+	public String updatedAt;
 
-	public Integer getId() {
-		return this.id;
-	}
-
-	public void setName(String name) {
+	public Comments(String name, String title, String comment, String createdAt, String updatedAt) {
 		this.name = name;
-	}
-
-	public String getName() {
-		return this.name;
-	}
-
-	public void setTitle(String title) {
 		this.title = title;
-	}
-
-	public String getTitle() {
-		return this.title;
-	}
-
-	public void setComment(String comment) {
 		this.comment = comment;
-	}
-
-	public String getComment() {
-		return this.comment;
-	}
-
-	public void setCreatedAt(String createdAt) {
 		this.createdAt = createdAt;
-	}
-
-	public String getCreatedAt() {
-		return this.createdAt;
-	}
-
-	public void setUpdatedAt(String updatedAt) {
 		this.updatedAt = updatedAt;
-	}
-
-	public String getUpdatedAt() {
-		return this.updatedAt;
 	}
 }
