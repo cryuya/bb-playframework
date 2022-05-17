313bd3828b823132f6908df8bd143bbec31d81ae
cryuya <r-sasaki@croooober.co.jp>
2022-05-17 17:27:35 +0900

    sql変更

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -5,7 +5,7 @@
 CREATE TABLE comments (
 	id int NOT NULL AUTO_INCREMENT,
 	name varchar(20) NOT NULL,
-	title varchar(255) NOT NULL,
+	title varchar(30) NOT NULL,
 	comment varchar(255) NOT NULL,
 	created_at TIMESTAMP NOT NULL,
 	updated_at TIMESTAMP NOT NULL,
