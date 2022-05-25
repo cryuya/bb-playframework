a0db81881eca34e7a182adc25d97e85d1a3ec3e2
cryuya <r-sasaki@croooober.co.jp>
2022-05-25 10:05:20 +0900

    usersモデルの追加

---
+++ app/models/Users.java
@@ -0,0 +1,23 @@
+package models;
+
+import play.data.validation.Constraints;
+import io.ebean.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+@Entity
+public class Users extends Model {
+	@Id
+	public Integer id;
+	@Constraints.Required 
+	public String name;
+	@Constraints.Required 
+	public String password;
+
+	public Users(String name, String password) {
+		this.name = name;
+		this.password = password;
+	}
+}
