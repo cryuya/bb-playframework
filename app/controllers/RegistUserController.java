0b662a7d68cd77c96cc4da1f9da446b9499358cd
cryuya <r-sasaki@croooober.co.jp>
2022-05-30 00:46:02 +0900

    登録処理の制御、登録ページの装飾追加

--- app/controllers/RegistUserController.java
+++ app/controllers/RegistUserController.java
@@ -35,9 +35,29 @@ public class RegistUserController extends Controller {
 		Form<UserData> boundForm = form.bindFromRequest(request);
 		UserData data = boundForm.get();
 		
-		Users u = new Users(data.getName(), data.getPassword());
-		u.save();
-
-		return redirect("/").flashing("info", "Regited user!");
+		if (boundForm.hasErrors()) {
+			return badRequest(views.html.login.render(this.form, request, messagesApi.preferred(request)));
+		} else {
+			Users searchedUser = 
+				finder.query()
+					.where()
+						.eq("name", data.getName())
+					.findOne();
+			
+				if (searchedUser == null) {
+					Users u = new Users(data.getName(), data.getPassword());
+					u.save();
+		
+					return 
+						redirect("/")
+							.addingToSession(request, "id", String.valueOf(data.getId()))
+							.addingToSession(request, "id", data.getName())
+							.flashing("", "registed user");
+				} else {
+					return 
+						redirect("/registUser")
+							.flashing("", "failure");
+				}
+		}
 	}
 } 
