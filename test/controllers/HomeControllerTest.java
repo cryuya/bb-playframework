2086014496494612a701772bf186efc3fa84ab9d
cryuya <r-sasaki@croooober.co.jp>
2022-06-07 17:29:51 +0900

    TopPageTest.java追加

--- test/controllers/HomeControllerTest.java
+++ test/controllers/HomeControllerTest.java
@@ -1,31 +1,4 @@
 package controllers;
 
-import org.junit.Test;
-import play.Application;
-import play.inject.guice.GuiceApplicationBuilder;
-import play.mvc.Http;
-import play.mvc.Result;
-import play.test.WithApplication;
-
-import static org.junit.Assert.assertEquals;
-import static play.mvc.Http.Status.BAD_REQUEST;
-import static play.test.Helpers.GET;
-import static play.test.Helpers.route;
-
-public class HomeControllerTest extends WithApplication {
-
-    @Override
-    protected Application provideApplication() {
-        return new GuiceApplicationBuilder().build();
-    }
-
-    @Test
-    public void トップページ表示のテスト() {
-        Http.RequestBuilder request = new Http.RequestBuilder()
-                .method(GET)
-                .uri("/");
-
-        Result result = route(app, request);
-        assertEquals(BAD_REQUEST, result.status());
-    }
+public class HomeControllerTest {
 }
